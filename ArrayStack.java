c27352b3eddf5dc5d84357448a8956210880e29e
khushichetty2007 <117398885+khushichetty2007@users.noreply.github.com>
2024-03-04 10:07:24 -0800

    Add files via upload

---
+++ ArrayStack.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.EmptyStackException;
+/**
+ * Simple stack using ArrayList
+ * 
+ * @author Khushi Chetty
+ * @since Feb 27 2024
+
+*/
+
+public class ArrayStack<E> implements Stack<E>
+{
+	private List<E> theStack;
+	public ArrayStack()
+	{
+		theStack = new ArrayList<E>();
+	}
+	/** @return true if the stack is empty fals otherwise*/
+	public boolean isEmpty (){return theStack.isEmpty();}
+	
+	/** @return the top elemnt on the stack */
+	public E peek()
+	{
+		if (theStack.isEmpty())
+			throw new EmptyStackException();
+		return theStack.get(theStack.size()-1);
+	}
+	
+	/** @param obj   the object to put on the top of the stack*/
+	public void push(E obj ){ theStack.add(obj);}
+	/** @return   the object removed fromt he top of the stack*/
+	public E pop()
+	{
+		if (theStack.isEmpty())
+			throw new EmptyStackException();
+			
+		return theStack.remove(theStack.size()-1);
+	}
+}
